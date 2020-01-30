f90e0b99b0b5346dd6f3f4e52a20572ef664f56c
bobylito <bobylito@gmail.com>
2020-01-30 17:39:45 +0100

    feat(website): add an RSS feed with image

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -92,6 +92,62 @@ module.exports = {
         display: "swap",
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allFile } }) => {
+              return allFile.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  title: edge.node.relativeDirectory,
+                  description: edge.node.relativeDirectory,
+                  date: edge.node.birthTime,
+                  url:
+                    site.siteMetadata.siteUrl +
+                    "/sketches/" +
+                    edge.node.relativeDirectory,
+                  custom_elements: [
+                    {
+                      "content:encoded": `<![CDATA[ <img src="${site.siteMetadata.siteUrl}${edge.node.publicURL}" /> ]]>`,
+                    },
+                  ],
+                })
+              })
+            },
+            query: `
+query getAllVideo {
+  allFile(filter: {name: {eq: "out"} extension:{eq:"mp4"}}, sort: {fields: relativeDirectory, order: DESC}) {
+    edges {
+      node {
+        name
+        birthTime
+        extension
+        relativeDirectory
+publicURL
+      }
+    }
+  }
+}
+            `,
+            output: "/rss.xml",
+            title: "Creative Code Daily by Bobylito | RSS feed",
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
