0677cd685b6284543ff27edda24ce6873d60b6bc
bobylito <bobylito@gmail.com>
2020-02-12 15:57:34 +0100

    feat(rss): update image and read title from readme

--- website/config/gatsby/feed.js
+++ website/config/gatsby/feed.js
@@ -16,11 +16,17 @@ module.exports = [
         `,
       feeds: [
         {
-          serialize: ({ query: { site, allFile } }) => {
+          serialize: ({ query: { site, allFile, markdown } }) => {
+            const titleByFolder = markdown.edges.reduce((m, edge) => {
+              m[edge.node.relativeDirectory] =
+                edge.node.childMarkdownRemark.frontmatter.title
+              return m
+            }, {})
             return allFile.edges.map(edge => {
-              return Object.assign({}, edge.node.frontmatter, {
-                title: edge.node.relativeDirectory,
-                description: edge.node.relativeDirectory,
+              const title = titleByFolder[edge.node.relativeDirectory]
+              return {
+                title: title || edge.node.relativeDirectory,
+                description: title || edge.node.relativeDirectory,
                 date: edge.node.birthTime,
                 url:
                   site.siteMetadata.siteUrl +
@@ -31,12 +37,24 @@ module.exports = [
                     "content:encoded": `<![CDATA[ <img src="${site.siteMetadata.siteUrl}${edge.node.publicURL}" /> ]]>`,
                   },
                 ],
-              })
+              }
             })
           },
           query: `
 query getAllVideo {
-  allFile(filter: {name: {eq: "out"} extension:{eq:"mp4"}}, sort: {fields: relativeDirectory, order: DESC}) {
+  markdown: allFile(filter: {extension: {eq: "md"}, sourceInstanceName: {eq: "source"}}, sort: {order: DESC, fields: relativeDirectory}) {
+    edges {
+      node {
+        relativeDirectory
+        childMarkdownRemark {
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+  allFile(filter: {name: {eq: "screenshot-2"} extension:{eq:"png"}}, sort: {fields: relativeDirectory, order: DESC}) {
     edges {
       node {
         name
