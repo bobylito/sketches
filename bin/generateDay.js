a2357828b99234221c767fd14b80569029c2fa73
bobylito <bobylito@gmail.com>
2020-04-24 22:07:38 +0200

    misc(dev): add generator script

---
+++ bin/generateDay.js
@@ -0,0 +1,22 @@
+const shell = require("shelljs");
+const { join, basename } = require("path");
+
+const p = join(process.cwd(), "code", "*");
+const lastDayFolder = shell
+  .ls("-d", p)
+  .filter(f => f.indexOf("day") != -1)
+  .reverse()[0];
+
+const lastDay = parseInt(basename(lastDayFolder).replace("day", ""));
+const today = lastDay + 1;
+
+const targetFolder = join(process.cwd(), "code", `day${today}`);
+const templateFolder = join(process.cwd(), "code", "templates", "shader");
+
+shell.cp("-R", templateFolder, targetFolder);
+
+// By convention, the PDE should have the folder name
+shell.mv(
+  join(targetFolder, "sketch.pde"),
+  join(targetFolder, `day${today}.pde`)
+);
